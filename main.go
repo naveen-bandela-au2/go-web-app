05a6ba264b32c991a2363f3c94de22b80481994d
naveen-bandela-au2 <b.naveen2085@gmail.com>
2026-04-21 16:55:41 +0530

    Change home route from '/home' to '/home1'

--- main.go
+++ main.go
@@ -27,7 +27,7 @@ func contactPage(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 
-	http.HandleFunc("/home", homePage)
+	http.HandleFunc("/home1", homePage)
 	http.HandleFunc("/courses", coursePage)
 	http.HandleFunc("/about", aboutPage)
 	http.HandleFunc("/contact", contactPage)
